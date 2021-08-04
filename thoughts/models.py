3376b544d864948fea9c74857d747ac5f07b6715
tvey <tanyavoid@ya.ru>
2021-08-04 22:39:18 +0300

    Create models

--- thoughts/models.py
+++ thoughts/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Thought(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    text = models.TextField()
+    is_editable = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
