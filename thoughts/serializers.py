4dfcf48785f0aeb88a7b84be0913b8db19796490
tvey <tanyavoid@ya.ru>
2021-08-06 22:36:52 +0300

    Add serializers

--- thoughts/serializers.py
+++ thoughts/serializers.py
@@ -1,1 +1,29 @@
 from rest_framework import serializers
+
+from .models import Thought, Tag, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    thought_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'thought_count']
+
+    def get_thought_count(self, obj):
+        return Thought.objects.filter(author=obj).count()
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+
+
+class ThoughtSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    tags = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Thought
+        fields = ['id', 'text', 'author', 'tags']
