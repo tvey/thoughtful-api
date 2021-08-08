094dbb8ba7119b31d5802a1ac942e88c9c6b9da4
tvey <tanyavoid@ya.ru>
2021-08-08 22:01:08 +0300

    Show models in admin and add str repr

--- thoughts/admin.py
+++ thoughts/admin.py
@@ -1,1 +1,29 @@
 from django.contrib import admin
+
+from .models import Thought, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'get_thought_count']
+
+    def get_thought_count(self, obj):
+        return Thought.objects.filter(tags__in=[obj.id]).count()
+
+    get_thought_count.short_description = 'Thought count'
+
+
+@admin.register(Thought)
+class ThoughtAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'get_text', 'is_editable', 'get_tag_count']
+    list_display_links = ['get_text']
+    ordering = ['id']
+
+    def get_text(self, obj):
+        return f'{obj.text[:50]}...' if len(obj.text) > 50 else obj.text
+
+    def get_tag_count(self, obj):
+        return Tag.objects.filter(thought__in=[obj.id]).count()
+
+    get_tag_count.short_description = 'Tag count'
+    get_text.short_description = 'Text'
--- thoughts/models.py
+++ thoughts/models.py
@@ -7,9 +7,15 @@ User = get_user_model()
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Thought(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     text = models.TextField()
     is_editable = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f'Thought {self.id} by {self.author}'
