fa11a74dd821bf4b1013d4c47c1347e8a2f42efa
tvey <tanyavoid@ya.ru>
2021-08-08 22:03:07 +0300

    Add basic views and routes

--- thoughts/urls.py
+++ thoughts/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 
-urlpatterns = [
+from rest_framework import routers
 
+from .views import ThoughtViewSet, list_tags, list_authors
+
+urlpatterns = [
+    path('tags/', list_tags, name='tags'),
+    path('authors/', list_authors, name='authors'),
 ]
+
+router = routers.SimpleRouter()
+router.register(r'thoughts', ThoughtViewSet)
+urlpatterns += router.urls
--- thoughts/views.py
+++ thoughts/views.py
@@ -1,1 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Thought, User, Tag
+from .serializers import ThoughtSerializer, TagSerializer, AuthorSerializer
+
+
+class ThoughtViewSet(viewsets.ModelViewSet):
+    """A simple ViewSet for viewing and editing thoughts."""
+
+    queryset = Thought.objects.all()
+    serializer_class = ThoughtSerializer
+
+
+@api_view(['GET'])
+def list_tags(request):
+    serializer = TagSerializer(Tag.objects.all(), many=True)
+    return Response({'tags': serializer.data})
+
+
+@api_view(['GET'])
+def list_authors(request):
+    serializer = AuthorSerializer(User.objects.all(), many=True)
+    return Response({'authors': serializer.data})
+
+
+# To-do: add users views and permissions, add and customize actions
