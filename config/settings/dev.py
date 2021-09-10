89ca4d4640682706da01c52dcfaa979174b50849
tvey <tanyavoid@ya.ru>
2021-09-10 23:59:05 +0300

    Local development settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -9,3 +9,10 @@ DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
