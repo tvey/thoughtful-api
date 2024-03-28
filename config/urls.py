6f26cd935cbd89bdee495cf0b5e9b2c4e71cbad1
tvey <liteitt@gmail.com>
2024-03-28 22:24:59 +0300

    Add Swagger with drf-yasg

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Thoughtful API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include('thoughts.urls')),
     path('', include('rest_framework.urls')),  # login/logout for browsable
     path('', include('users.urls')),
